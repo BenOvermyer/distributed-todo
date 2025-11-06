fdd3b56441d415920d0968dc336db5ed351f64fc
Ben Overmyer <ben@overmyer.net>
2025-11-06 10:03:42 -0330

    Add complete/uncomplete, break up some big functions, and add more tests

---
+++ common/task.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+"""
+This module defines the Task data structure.
+
+We are not using class methods for stateful operations because
+we want to decouple data representation and datastore operations.
+This makes it easier to test and debug.
+"""
+
+@dataclass # Why dataclass? Because it's my closest approximation to Typescript types.
+class Task:
+    id: int
+    username: str
+    content: str
+    is_completed: bool
+    due_date: str | None
+    created_at: str
+    updated_at: str
